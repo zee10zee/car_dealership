f94fe20ba73e1a4449a3a2a8a87abce8d7e50348
zee10zee <=>
2026-05-03 20:09:35 +0430

    first commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const AuthLayout = () => {
+ return <Stack 
+  screenOptions={{headerShown: false, headerBackButtonDisplayMode : 'minimal'}}
+  />
+
+}
+
+export default AuthLayout
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const TabsLayout = () => {
+  return <Stack screenOptions={{headerShown : false}}/>
+}
+
+export default TabsLayout
+
+const styles = StyleSheet.create({})
