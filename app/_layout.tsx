67e3a3e4510b4fb29cbdc92c892814e091c4c655
zee10zee <=>
2026-05-04 21:35:00 +0430

    pulled from dev

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
-
+import {StatusBar} from 'expo-status-bar'
 export default function RootLayout() {
   return (
   <Stack screenOptions={{headerShown : false}}>
 
     <Stack.Screen name="(tabs)" />
+    <StatusBar style="dark" />
 
   </Stack>
   )
